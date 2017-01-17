ed62a11a0ddc59cf78b5f776ec936e72b6174cf0
Nissar Chababy <funilrys@outlook.com>
2017-01-17 01:13:43 +0100

    Add Error & Exceptions Handler

---
+++ Core/Error.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Nissar Chababy <contact at funilrys dot com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace Core;
+
+/**
+ * Error & Exception Handler
+ *
+ * @author Nissar Chababy <contact at funilrys dot com>
+ */
+class Error
+{
+
+    /**
+     * Error handler. Convert all errors to Exceptions by throwing an ErrorException.
+     *
+     * @param int $level  Error level
+     * @param string $message  Error message
+     * @param string $file  Filename the error was raised in
+     * @param int $line  Line number in the file
+     *
+     * @return void
+     */
+    public static function errorHandler($level, $message, $file, $line)
+    {
+        if (error_reporting() !== 0) {  // to keep the @ operator working
+            throw new \ErrorException($message, 0, $level, $file, $line);
+        }
+    }
+
+    /**
+     * Exception handler.
+     *
+     * @param Exception $exception  The exception
+     *
+     * @return void
+     */
+    public static function exceptionHandler($exception)
+    {
+        // Code is 404 (not found) or 500 (general error)
+        $code = $exception->getCode();
+        if ($code != 404) {
+            $code = 500;
+        }
+        http_response_code($code);
+
+        if (\App\Config\Errors::SHOW_ERRORS) {
+            echo "<h1>Fatal error</h1>";
+            echo "<p>Uncaught exception: '" . get_class($exception) . "'</p>";
+            echo "<p>Message: '" . $exception->getMessage() . "'</p>";
+            echo "<p>Stack trace:<pre>" . $exception->getTraceAsString() . "</pre></p>";
+            echo "<p>Thrown in '" . $exception->getFile() . "' on line " . $exception->getLine() . "</p>";
+        } else {
+            $log = dirname(__DIR__) . '/App/logs/' . date('Y-m-d') . '.txt';
+            ini_set('error_log', $log);
+
+            $message = "Uncaught exception: '" . get_class($exception) . "'";
+            $message .= " with message '" . $exception->getMessage() . "'";
+            $message .= "\nStack trace: " . $exception->getTraceAsString();
+            $message .= "\nThrown in '" . $exception->getFile() . "' on line " . $exception->getLine();
+
+            error_log($message);
+
+            View::renderTemplate("$code.html");
+        }
+    }
+
+}
