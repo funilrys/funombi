1221bd0c9c9b368dbac09d6c668804f51ab65499
Nissar Chababy <funilrys@outlook.com>
2017-01-17 01:11:06 +0100

    Add Core/Controller.php + Create __call to call before() and after() which can be used in every Controllers

---
+++ Core/Controller.php
@@ -0,0 +1,100 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Nissar Chababy <contact at funilrys dot com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace Core;
+
+/**
+ * Base Controller
+ *
+ * @author Nissar Chababy <contact at funilrys dot com>
+ */
+abstract class Controller
+{
+
+    /**
+     * Parameters from the matched route
+     * @var array
+     */
+    protected $route_params = [];
+
+    /**
+     * Class constructor
+     *
+     * @param array $route_params  Parameters from the route
+     *
+     * @return void
+     */
+    public function __construct($route_params)
+    {
+        $this->route_params = $route_params;
+    }
+
+    /**
+     * Magic method called when a non-existent or inaccessible method is
+     * called on an object of this class. It's Used to execute before and after
+     * filter methods on action methods. Action methods need to be named
+     * with an "Action" suffix, e.g. indexAction, showAction etc.
+     *
+     * @param string $name  Method name
+     * @param array $args Arguments passed to the method
+     *
+     * @return void
+     */
+    public function __call($name, $args)
+    {
+        $method = $name . 'Action';
+
+        if (method_exists($this, $method)) {
+            if ($this->before() !== false) {
+                call_user_func_array([$this, $method], $args);
+                $this->after();
+            }
+        } else {
+            throw new \Exception("Method $method not found in controller " . get_class($this));
+        }
+    }
+
+    /**
+     * Before filter - called before an action method.
+     *
+     * @return void
+     */
+    protected function before()
+    {
+        
+    }
+
+    /**
+     * After filter - called after an action method.
+     *
+     * @return void
+     */
+    protected function after()
+    {
+        
+    }
+
+}
