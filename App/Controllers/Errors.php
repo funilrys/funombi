d7032bff4ff7144d2af4a9dddf18f2299206597c
Nissar Chababy <funilrys@outlook.com>
2017-01-18 05:00:00 +0100

    Add link to error views

---
+++ App/Controllers/Errors.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * The MIT License
+ *
+ * Copyright 2017 Nissar Chababy <contact at funilrys dot com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+namespace App\Controllers;
+
+/**
+ * Errors controller
+ *
+ * @author Nissar Chababy <contact at funilrys dot com>
+ */
+class Errors
+{
+
+    public function forbidden()
+    {
+        View::renderTemplate('Errors/403.html.twig');
+    }
+
+    public function notFound()
+    {
+        View::renderTemplate('Errors/404.html.twig');
+    }
+
+    public function internalServerError()
+    {
+        View::renderTemplate('Errors/500.html.twig');
+    }
+
+    public function badGateway()
+    {
+         View::renderTemplate('Errors/502.html.twig');
+    }
+
+    public function serviceUnavailable()
+    {
+         View::renderTemplate('Errors/503.html.twig');
+    }
+
+    public function gatewayTimeout()
+    {
+         View::renderTemplate('Errors/504.html.twig');
+    }
+
+}
