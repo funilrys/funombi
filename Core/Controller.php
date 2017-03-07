883e0be7e4e389ca6368a5938e58d1af27164910
Nissar Chababy <funilrys@outlook.com>
2017-03-07 16:06:05 +0100

    Remove ::showDebugger()

--- Core/Controller.php
+++ Core/Controller.php
@@ -73,8 +73,6 @@ class Controller
             Session::startSession();
 
             if ($this->before() !== false) {
-                Error::showDebugger();
-
                 call_user_func_array([$this, $method], $args);
                 $this->after();
             }
