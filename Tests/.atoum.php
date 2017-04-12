8feeb5ddc68113c62b01329c0db666c4c8d40521
funilrys <contact@funilrys.com>
2017-04-12 09:20:40 +0200

    Update autoloader

--- Tests/.atoum.php
+++ Tests/.atoum.php
@@ -1,16 +1,23 @@
 <?php
 
 /*
-This file will automatically be included before EACH run.
+  This file will automatically be included before EACH run.
 
-Use it to configure atoum or anything that needs to be done before EACH run.
+  Use it to configure atoum or anything that needs to be done before EACH run.
 
-More information on documentation:
-[en] http://docs.atoum.org/en/latest/chapter3.html#configuration-files
-[fr] http://docs.atoum.org/fr/latest/lancement_des_tests.html#fichier-de-configuration
-*/
+  More information on documentation:
+  [en] http://docs.atoum.org/en/latest/chapter3.html#configuration-files
+  [fr] http://docs.atoum.org/fr/latest/lancement_des_tests.html#fichier-de-configuration
+ */
 
-require_once dirname(__DIR__,1).'/vendor/autoload.php';
+$autoloader = dirname(__DIR__, 1) . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+if (is_file($autoloader)) {
+    require $autoloader;
+} else {
+    echo 'Please install the dependencies with "composer update". If this message persist, please check the permissions of your current project.';
+    exit();
+}
 
 use \mageekguy\atoum;
 
