a9f1a824e93382b02b2d1403957796044c54696e
funilrys <contact@funilrys.com>
2017-04-17 10:20:35 +0200

    Add code coverage + Add logo

--- Tests/.atoum.php
+++ Tests/.atoum.php
@@ -19,58 +19,19 @@ if (is_file($autoloader)) {
     exit();
 }
 
-use \mageekguy\atoum;
+use mageekguy\atoum;
 
 $report = $script->addDefaultReport();
 
-/*
-LOGO
+$coverageField = new atoum\report\fields\runner\coverage\html('Funombi', 'Tests/coverage');
+//$coverageField->setRootUrl('http://url/of/web/site');
+
+$report->addField($coverageField);
 
 // This will add the atoum logo before each run.
 $report->addField(new atoum\report\fields\runner\atoum\logo());
 
 // This will add a green or red logo after each run depending on its status.
 $report->addField(new atoum\report\fields\runner\result\logo());
-*/
 
-/*
-CODE COVERAGE SETUP
-
-// Please replace in next line "Project Name" by your project name and "/path/to/destination/directory" by your destination directory path for html files.
-$coverageField = new atoum\report\fields\runner\coverage\html('Project Name', '/path/to/destination/directory');
-
-// Please replace in next line http://url/of/web/site by the root url of your code coverage web site.
-$coverageField->setRootUrl('http://url/of/web/site');
-
-$report->addField($coverageField);
-*/
-
-/*
-TEST EXECUTION SETUP
-
-// Please replace in next line "/path/to/your/tests/units/classes/directory" by your unit test's directory.
-$runner->addTestsFromDirectory('path/to/your/tests/units/classes/directory');
-*/
-
-/*
-TEST GENERATOR SETUP
-
-$testGenerator = new atoum\test\generator();
-
-// Please replace in next line "/path/to/your/tests/units/classes/directory" by your unit test's directory.
-$testGenerator->setTestClassesDirectory('path/to/your/tests/units/classes/directory');
-
-// Please replace in next line "your\project\namespace\tests\units" by your unit test's namespace.
-$testGenerator->setTestClassNamespace('your\project\namespace\tests\units');
-
-// Please replace in next line "/path/to/your/classes/directory" by your classes directory.
-$testGenerator->setTestedClassesDirectory('path/to/your/classes/directory');
-
-// Please replace in next line "your\project\namespace" by your project namespace.
-$testGenerator->setTestedClassNamespace('your\project\namespace');
-
-// Please replace in next line "path/to/your/tests/units/runner.php" by path to your unit test's runner.
-$testGenerator->setRunnerPath('path/to/your/tests/units/runner.php');
-
-$script->getRunner()->setTestGenerator($testGenerator);
-*/
+$runner->addTestsFromDirectory('Tests');
