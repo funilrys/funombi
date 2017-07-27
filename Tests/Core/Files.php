67c1a489301c7f885c1cbc34465632dcbc3846a7
funilrys <contact@funilrys.com>
2017-07-27 19:48:20 +0200

    Fix version

--- Tests/Core/Files.php
+++ Tests/Core/Files.php
@@ -29,7 +29,7 @@
  *
  * @package     funombi\Core\tests
  * @author      Nissar Chababy <contact at funilrys dot com>
- * @version     0.0.1
+ * @version     1.0.0
  * @copyright   Copyright (c) 2017, Nissar Chababy
  */
 
