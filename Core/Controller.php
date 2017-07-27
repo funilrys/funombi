37d32c6b3298ee3fadeb9b5f1dbac84b2d9b0402
funilrys <contact@funilrys.com>
2017-07-27 18:40:19 +0200

    Fix PHPDOC path

--- Core/Controller.php
+++ Core/Controller.php
@@ -25,7 +25,7 @@
  */
 
 /**
- * Class Controller | App/Core/Controller.php
+ * Class Controller | Core/Controller.php
  *
  * @package     funombi\Core
  * @author      Nissar Chababy <contact at funilrys dot com>
